5cdd3ab5cd6075bdf2d30e8311c8989cb4bf2b52
dajining <odajima.yu+github@gmail.com>
2018-04-23 20:41:03 +0900

    a

---
+++ src/pages/menu.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+export default class MenuPage extends React.PureComponent {
+  render () {
+    return (
+      <div style={this.props.transition && this.props.transition.style}>
+        <h1>animals</h1>
+        <div>
+          <Link to='/'>HOME</Link>
+        </div>
+        <div>
+          <Link to="/dog/">Go to dog</Link>
+        </div>
+        <div>
+          <Link to="/long-page/">Go to long page</Link>
+        </div>
+      </div>
+    )
+  }
+}
